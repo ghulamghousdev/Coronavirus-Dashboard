927307b045ec46c1f9783b115d1d07aca52329d3
ghulamghousdev <itsghulamghous@gmail.com>
2020-04-08 12:49:12 +0500

    📦 NEW: First Version

---
+++ src/stringToColor.js
@@ -0,0 +1,18 @@
+import stc from "../web_modules/string-to-color.js";
+
+export const stringToHex = stc;
+
+function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
+    : "";
+}
+
+export function stringToRGB(str) {
+  return hexToRgb(stc(str));
+}
