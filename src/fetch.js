b4872a5b24f0f4e8880bc972c1f80f6c16eadddc
ghulamghousdev <itsghulamghous@gmail.com>
2020-04-08 12:29:21 +0500

    📦 NEW: First Version

---
+++ src/fetch.js
@@ -0,0 +1,18 @@
+import { addCustomStatsToReport } from "./stats.js";
+import { request } from "../web_modules/@hyperapp/http.js";
+import { update } from "./update.js";
+
+const GotReport = (state, report) =>
+  update({
+    ...state,
+    report: addCustomStatsToReport({
+      report,
+      reportType: state.reportType,
+      days: state.days
+    })
+  });
+export const fetchReport = request({
+  url: "https://pomber.github.io/covid19/timeseries.json",
+  expect: "json",
+  action: GotReport
+});
