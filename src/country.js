c052835b8b2952083e19a5f1c57ea1cc7248df63
ghulamghousdev <itsghulamghous@gmail.com>
2020-04-08 12:56:47 +0500

    📦 NEW: First Version

---
+++ src/country.js
@@ -0,0 +1,65 @@
+import { stringToHex } from "./stringToColor.js";
+import { html } from "./html.js";
+import { update } from "./update.js";
+
+const unique = list => [...new Set(list)];
+export const AddCountry = currentCountry => state =>
+  update({
+    ...state,
+    currentCountry,
+    selectedCountries: unique([...state.selectedCountries, currentCountry])
+  });
+
+export const RemoveCountry = country => state =>
+  update({
+    ...state,
+    selectedCountries: state.selectedCountries.filter(c => c !== country)
+  });
+
+export const isActive = ({ selectedCountries }) => country =>
+  selectedCountries.includes(country);
+
+export const countryAction = state => country => {
+  const isCountryActive = isActive(state)(country);
+
+  if (isCountryActive) {
+    return RemoveCountry(country);
+  } else {
+    return AddCountry(country);
+  }
+};
+
+export const countryHighlight = state => country => {
+  const isCountryActive = isActive(state)(country);
+
+  if (isCountryActive) {
+    return { "font-weight": "bold", color: stringToHex(country) };
+  } else {
+    return {};
+  }
+};
+
+export const countryChip = name => state => html`
+  <span
+    class="chip c-hand"
+    onclick=${countryAction(state)(name)}
+    style=${countryHighlight(state)(name)}
+  >
+    ${name}
+    <span
+      class="btn btn-clear"
+      href="#"
+      aria-label="Remove Country"
+      role="button"
+    ></span>
+  </span>
+`;
+
+export const countryChips = state =>
+  html`
+    <div class="m-2">
+      ${Array.from(state.selectedCountries).map(name =>
+        countryChip(name)(state)
+      )}
+    </div>
+  `;
